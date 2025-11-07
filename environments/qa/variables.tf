64fcdfa0bdd998080b7808feea8e81080a0ab1af
rohitreddy21122000 <rohitreddy21122000@gmail.com>
2025-11-08 00:05:41 +0530

    updated qa realted variables.tf

--- environments/qa/variables.tf
+++ environments/qa/variables.tf
@@ -1,29 +1,106 @@
-variable "region" {
-  description = "AWS region"
+variable "env" {
+  description = "Deployment environment (qa, uat, prod)"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy resources"
   type        = string
   default     = "ap-south-1"
 }
 
-variable "environment" {
-  description = "Environment name"
-  type        = string
-  default     = "qa"
+variable "rate_limit" {
+  description = "Rate limit for WAF protection"
+  type        = number
+  default     = 2000
 }
 
-variable "lambda_timeout" {
-  description = "Lambda function timeout in seconds"
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+}
+
+variable "enable_versioning" {
+  description = "Enable S3 versioning"
+  type        = bool
+  default     = true
+}
+
+variable "document_retention_days" {
+  description = "S3 object retention period in days"
   type        = number
   default     = 30
 }
 
-variable "lambda_memory_size" {
-  description = "Lambda function memory size in MB"
-  type        = number
-  default     = 512
+variable "enable_intelligent_tiering" {
+  description = "Enable S3 intelligent tiering"
+  type        = bool
+  default     = false
 }
 
-variable "waf_rate_limit" {
-  description = "WAF rate limit per 5 minutes"
+variable "tags" {
+  description = "Common tags applied to all resources"
+  type        = map(string)
+  default = {
+    Project     = "TechAutomation"
+    Environment = "qa"
+    ManagedBy   = "Terraform"
+  }
+}
+
+variable "function_name" {
+  description = "Lambda function name"
+  type        = string
+  default     = "jira-webhook-handler"
+}
+
+variable "runtime" {
+  description = "Lambda runtime version"
+  type        = string
+  default     = "python3.9"
+}
+
+variable "memory_size" {
+  description = "Lambda memory allocation in MB"
   type        = number
-  default     = 2000
+  default     = 256
+}
+
+variable "timeout" {
+  description = "Lambda timeout in seconds"
+  type        = number
+  default     = 30
+}
+
+variable "jira_api_token" {
+  description = "Jira API token"
+  type        = string
+  sensitive   = true
+}
+
+variable "jira_email" {
+  description = "Jira user email"
+  type        = string
+}
+
+variable "jira_base_url" {
+  description = "Jira base URL"
+  type        = string
+}
+
+variable "jira_connection_arn" {
+  description = "Jira connection ARN"
+  type        = string
+}
+
+variable "jira_webhook_secret" {
+  description = "Webhook secret for Jira validation"
+  type        = string
+  sensitive   = true
+}
+
+variable "state_definition_file" {
+  description = "Path to Step Function state definition JSON"
+  type        = string
+  default     = "state_machine_definition.json"
 }
